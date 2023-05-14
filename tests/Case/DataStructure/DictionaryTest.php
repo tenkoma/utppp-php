5c86007e750634541b4ede58f4844ec3a19cf931
Koji Tanaka <tenkoma@gmail.com>
2023-05-15 00:34:09 +0900

    feat: implement Dictionary Data Structure

---
+++ tests/Case/DataStructure/DictionaryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tenkoma\UtpppExample\Test\Case\DataStructure;
+
+use PHPUnit\Framework\TestCase;
+use Tenkoma\UtpppExample\Chapter2\Listing1\Product;
+use Tenkoma\UtpppExample\DataStructure\Dictionary;
+
+class DictionaryTest extends TestCase
+{
+    public function testSet(): void
+    {
+        $sut = new Dictionary();
+        $sut->set('key1', 1);
+        $this->assertSame(1, $sut->get('key1'));
+        $sut->set('key2', 1);
+        $this->assertSame(1, $sut->get('key2'));
+    }
+
+    public function testGetNonExistKey(): void
+    {
+        $sut = new Dictionary();
+
+        $this->expectException(\BadMethodCallException::class);
+        $sut->get('key');
+    }
+
+    public function testContainsKey(): void
+    {
+        $sut = new Dictionary();
+        $sut->set('exists-key', 1);
+        $this->assertTrue($sut->containsKey('exists-key'));
+        $this->assertFalse($sut->containsKey('non-exists-key'));
+    }
+}
