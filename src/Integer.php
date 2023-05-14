bb649e17a455494d223693e88fb8debbe143a47e
Koji Tanaka <tenkoma@gmail.com>
2023-05-14 20:22:41 +0900

    $ composer phpcbf

--- src/Integer.php
+++ src/Integer.php
@@ -6,17 +6,17 @@ namespace Tenkoma\UtpppExample;
 
 class Integer
 {
-	public function __construct(private int $value)
-	{
-	}
+    public function __construct(private int $value)
+    {
+    }
 
-	public function toInt(): int
-	{
-		return $this->value;
-	}
+    public function toInt(): int
+    {
+        return $this->value;
+    }
 
-	public function add(self $augend): self
-	{
-		return new self($this->value + $augend->value);
-	}
+    public function add(self $augend): self
+    {
+        return new self($this->value + $augend->value);
+    }
 }
--- tests/Unit/Case/IntegerTest.php
+++ tests/Unit/Case/IntegerTest.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Tenkoma\UtpppExample\Test\Unit\Case;
@@ -8,18 +9,18 @@ use Tenkoma\UtpppExample\Test\Unit\TestCase;
 
 class IntegerTest extends TestCase
 {
-	public function testToInt(): void
-	{
-		$sut = new Integer(1);
-		$this->assertSame(1, $sut->toInt());
+    public function testToInt(): void
+    {
+        $sut = new Integer(1);
+        $this->assertSame(1, $sut->toInt());
 
-		$sut = new Integer(2);
-		$this->assertSame(2, $sut->toInt());
-	}
+        $sut = new Integer(2);
+        $this->assertSame(2, $sut->toInt());
+    }
 
-	public function testAdd(): void
-	{
-		$sut = new Integer(1);
-		$this->assertSame(3, $sut->add(new Integer(2))->toInt());
-	}
+    public function testAdd(): void
+    {
+        $sut = new Integer(1);
+        $this->assertSame(3, $sut->add(new Integer(2))->toInt());
+    }
 }
