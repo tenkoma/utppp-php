26b57689e5cf157eb28b62812c50f81fe3fe1a21
Koji Tanaka <tenkoma@gmail.com>
2023-07-15 21:01:53 +0900

    feat: Chapter7-1 Example to Chapter7-2

---
+++ src/Chapter7/Listing2/Bus.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tenkoma\UtpppExample\Chapter7\Listing2;
+
+class Bus implements BusInterface
+{
+    public function send(string $string): void
+    {
+    }
+}
---
+++ src/Chapter7/Listing2/User.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tenkoma\UtpppExample\Chapter7\Listing2;
+
+class User
+{
+    private int $userId;
+    private string $email;
+    private UserType $type;
+
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getType(): UserType
+    {
+        return $this->type;
+    }
+
+    public function changeEmail(int $userId, string $newEmail): void
+    {
+        $data = Database::getUserById($userId);
+        $this->userId = $userId;
+        $this->email = (string)$data[1];
+        $this->type = UserType::from((string)$data[2]);
+
+        if ($this->email === $newEmail) {
+            return;
+        }
+
+        $companyData = Database::getCompany();
+        $companyDomainName = (string)$companyData[0];
+        $numberOfEmployees = (int)$companyData[1];
+
+        $emailDomain = ((array)mb_split('@', $newEmail))[1];
+        $isEmailCorporate = $emailDomain === $companyDomainName;
+        $newType = $isEmailCorporate
+            ? UserType::Employee
+            : UserType::Customer;
+
+        if ($this->type !== $newType) {
+            $delta = $newType === UserType::Employee ? 1 : -1;
+            $newNumber = $numberOfEmployees + $delta;
+            Database::saveCompany($newNumber);
+        }
+
+        $this->email = $newEmail;
+        $this->type = $newType;
+
+        Database::saveUser($this);
+        MessageBus::sendEmailChangedMessage($userId, $newEmail);
+    }
+}
